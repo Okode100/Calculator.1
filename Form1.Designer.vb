2eeebc512c32fe423f6c2314d1cbd55222d17042
Okode100 <mosesokode76@gmail.com>
2024-01-31 01:13:14 +0300

    Improvement
    Improvement

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.LCD = New System.Windows.Forms.TextBox()
         Me.n1 = New System.Windows.Forms.Button()
@@ -33,14 +33,22 @@ Partial Class Form1
         Me.n8 = New System.Windows.Forms.Button()
         Me.n9 = New System.Windows.Forms.Button()
         Me.n0 = New System.Windows.Forms.Button()
+        Me.bc = New System.Windows.Forms.Button()
+        Me.bdiv = New System.Windows.Forms.Button()
+        Me.bmult = New System.Windows.Forms.Button()
+        Me.bsub = New System.Windows.Forms.Button()
+        Me.bad = New System.Windows.Forms.Button()
+        Me.bequal = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'LCD
         '
         Me.LCD.BackColor = System.Drawing.Color.White
-        Me.LCD.Location = New System.Drawing.Point(56, 12)
+        Me.LCD.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LCD.Location = New System.Drawing.Point(7, 12)
+        Me.LCD.Multiline = True
         Me.LCD.Name = "LCD"
-        Me.LCD.Size = New System.Drawing.Size(555, 20)
+        Me.LCD.Size = New System.Drawing.Size(189, 34)
         Me.LCD.TabIndex = 0
         Me.LCD.Text = "0"
         Me.LCD.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
@@ -50,9 +58,9 @@ Partial Class Form1
         Me.n1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.n1.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.n1.ForeColor = System.Drawing.Color.White
-        Me.n1.Location = New System.Drawing.Point(56, 60)
+        Me.n1.Location = New System.Drawing.Point(6, 52)
         Me.n1.Name = "n1"
-        Me.n1.Size = New System.Drawing.Size(75, 40)
+        Me.n1.Size = New System.Drawing.Size(45, 40)
         Me.n1.TabIndex = 1
         Me.n1.Text = "1"
         Me.n1.UseVisualStyleBackColor = False
@@ -62,9 +70,9 @@ Partial Class Form1
         Me.n2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.n2.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.n2.ForeColor = System.Drawing.Color.White
-        Me.n2.Location = New System.Drawing.Point(155, 60)
+        Me.n2.Location = New System.Drawing.Point(57, 52)
         Me.n2.Name = "n2"
-        Me.n2.Size = New System.Drawing.Size(75, 40)
+        Me.n2.Size = New System.Drawing.Size(45, 40)
         Me.n2.TabIndex = 1
         Me.n2.Text = "2"
         Me.n2.UseVisualStyleBackColor = False
@@ -74,9 +82,9 @@ Partial Class Form1
         Me.n3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.n3.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.n3.ForeColor = System.Drawing.Color.White
-        Me.n3.Location = New System.Drawing.Point(267, 60)
+        Me.n3.Location = New System.Drawing.Point(108, 52)
         Me.n3.Name = "n3"
-        Me.n3.Size = New System.Drawing.Size(75, 40)
+        Me.n3.Size = New System.Drawing.Size(45, 40)
         Me.n3.TabIndex = 1
         Me.n3.Text = "3"
         Me.n3.UseVisualStyleBackColor = False
@@ -86,9 +94,9 @@ Partial Class Form1
         Me.n6.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.n6.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.n6.ForeColor = System.Drawing.Color.White
-        Me.n6.Location = New System.Drawing.Point(267, 136)
+        Me.n6.Location = New System.Drawing.Point(108, 98)
         Me.n6.Name = "n6"
-        Me.n6.Size = New System.Drawing.Size(75, 40)
+        Me.n6.Size = New System.Drawing.Size(45, 40)
         Me.n6.TabIndex = 1
         Me.n6.Text = "6"
         Me.n6.UseVisualStyleBackColor = False
@@ -98,9 +106,9 @@ Partial Class Form1
         Me.n5.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.n5.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.n5.ForeColor = System.Drawing.Color.White
-        Me.n5.Location = New System.Drawing.Point(155, 136)
+        Me.n5.Location = New System.Drawing.Point(57, 98)
         Me.n5.Name = "n5"
-        Me.n5.Size = New System.Drawing.Size(75, 40)
+        Me.n5.Size = New System.Drawing.Size(45, 40)
         Me.n5.TabIndex = 1
         Me.n5.Text = "5"
         Me.n5.UseVisualStyleBackColor = False
@@ -110,9 +118,9 @@ Partial Class Form1
         Me.n4.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.n4.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.n4.ForeColor = System.Drawing.Color.White
-        Me.n4.Location = New System.Drawing.Point(56, 136)
+        Me.n4.Location = New System.Drawing.Point(6, 98)
         Me.n4.Name = "n4"
-        Me.n4.Size = New System.Drawing.Size(75, 40)
+        Me.n4.Size = New System.Drawing.Size(45, 40)
         Me.n4.TabIndex = 1
         Me.n4.Text = "4"
         Me.n4.UseVisualStyleBackColor = False
@@ -122,9 +130,9 @@ Partial Class Form1
         Me.n7.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.n7.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.n7.ForeColor = System.Drawing.Color.White
-        Me.n7.Location = New System.Drawing.Point(56, 209)
+        Me.n7.Location = New System.Drawing.Point(6, 144)
         Me.n7.Name = "n7"
-        Me.n7.Size = New System.Drawing.Size(75, 40)
+        Me.n7.Size = New System.Drawing.Size(45, 40)
         Me.n7.TabIndex = 1
         Me.n7.Text = "7"
         Me.n7.UseVisualStyleBackColor = False
@@ -134,9 +142,9 @@ Partial Class Form1
         Me.n8.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.n8.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.n8.ForeColor = System.Drawing.Color.White
-        Me.n8.Location = New System.Drawing.Point(155, 209)
+        Me.n8.Location = New System.Drawing.Point(57, 144)
         Me.n8.Name = "n8"
-        Me.n8.Size = New System.Drawing.Size(75, 40)
+        Me.n8.Size = New System.Drawing.Size(45, 40)
         Me.n8.TabIndex = 1
         Me.n8.Text = "8"
         Me.n8.UseVisualStyleBackColor = False
@@ -146,9 +154,9 @@ Partial Class Form1
         Me.n9.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.n9.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.n9.ForeColor = System.Drawing.Color.White
-        Me.n9.Location = New System.Drawing.Point(267, 209)
+        Me.n9.Location = New System.Drawing.Point(108, 144)
         Me.n9.Name = "n9"
-        Me.n9.Size = New System.Drawing.Size(75, 40)
+        Me.n9.Size = New System.Drawing.Size(45, 40)
         Me.n9.TabIndex = 1
         Me.n9.Text = "9"
         Me.n9.UseVisualStyleBackColor = False
@@ -158,19 +166,98 @@ Partial Class Form1
         Me.n0.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.n0.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.n0.ForeColor = System.Drawing.Color.White
-        Me.n0.Location = New System.Drawing.Point(155, 281)
+        Me.n0.Location = New System.Drawing.Point(57, 190)
         Me.n0.Name = "n0"
-        Me.n0.Size = New System.Drawing.Size(75, 40)
+        Me.n0.Size = New System.Drawing.Size(45, 40)
         Me.n0.TabIndex = 1
         Me.n0.Text = "0"
         Me.n0.UseVisualStyleBackColor = False
+        '
+        'bc
+        '
+        Me.bc.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.bc.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bc.ForeColor = System.Drawing.Color.White
+        Me.bc.Location = New System.Drawing.Point(108, 190)
+        Me.bc.Name = "bc"
+        Me.bc.Size = New System.Drawing.Size(45, 40)
+        Me.bc.TabIndex = 1
+        Me.bc.Text = "C"
+        Me.bc.UseVisualStyleBackColor = False
+        '
+        'bdiv
+        '
+        Me.bdiv.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.bdiv.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bdiv.ForeColor = System.Drawing.Color.White
+        Me.bdiv.Location = New System.Drawing.Point(159, 190)
+        Me.bdiv.Name = "bdiv"
+        Me.bdiv.Size = New System.Drawing.Size(45, 40)
+        Me.bdiv.TabIndex = 1
+        Me.bdiv.Text = "/"
+        Me.bdiv.UseVisualStyleBackColor = False
+        '
+        'bmult
+        '
+        Me.bmult.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.bmult.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bmult.ForeColor = System.Drawing.Color.White
+        Me.bmult.Location = New System.Drawing.Point(159, 144)
+        Me.bmult.Name = "bmult"
+        Me.bmult.Size = New System.Drawing.Size(45, 40)
+        Me.bmult.TabIndex = 1
+        Me.bmult.Text = "*"
+        Me.bmult.UseVisualStyleBackColor = False
+        '
+        'bsub
+        '
+        Me.bsub.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.bsub.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bsub.ForeColor = System.Drawing.Color.White
+        Me.bsub.Location = New System.Drawing.Point(159, 98)
+        Me.bsub.Name = "bsub"
+        Me.bsub.Size = New System.Drawing.Size(45, 40)
+        Me.bsub.TabIndex = 1
+        Me.bsub.Text = "-"
+        Me.bsub.UseVisualStyleBackColor = False
+        '
+        'bad
+        '
+        Me.bad.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.bad.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bad.ForeColor = System.Drawing.Color.White
+        Me.bad.Location = New System.Drawing.Point(159, 52)
+        Me.bad.Name = "bad"
+        Me.bad.Size = New System.Drawing.Size(45, 40)
+        Me.bad.TabIndex = 1
+        Me.bad.Text = "+"
+        Me.bad.UseVisualStyleBackColor = False
+        '
+        'bequal
+        '
+        Me.bequal.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.bequal.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bequal.ForeColor = System.Drawing.Color.White
+        Me.bequal.Location = New System.Drawing.Point(6, 190)
+        Me.bequal.Name = "bequal"
+        Me.bequal.Size = New System.Drawing.Size(45, 40)
+        Me.bequal.TabIndex = 1
+        Me.bequal.Text = "="
+        Me.bequal.UseVisualStyleBackColor = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(642, 450)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(210, 238)
         Me.Controls.Add(Me.n9)
+        Me.Controls.Add(Me.bad)
+        Me.Controls.Add(Me.bsub)
+        Me.Controls.Add(Me.bmult)
+        Me.Controls.Add(Me.bdiv)
+        Me.Controls.Add(Me.bequal)
+        Me.Controls.Add(Me.bc)
         Me.Controls.Add(Me.n0)
         Me.Controls.Add(Me.n8)
         Me.Controls.Add(Me.n7)
@@ -181,6 +268,8 @@ Partial Class Form1
         Me.Controls.Add(Me.n2)
         Me.Controls.Add(Me.n1)
         Me.Controls.Add(Me.LCD)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MinimizeBox = False
         Me.Name = "Form1"
         Me.Text = "My calculator"
         Me.ResumeLayout(False)
@@ -199,4 +288,10 @@ Partial Class Form1
     Friend WithEvents n8 As Button
     Friend WithEvents n9 As Button
     Friend WithEvents n0 As Button
+    Friend WithEvents bc As Button
+    Friend WithEvents bdiv As Button
+    Friend WithEvents bmult As Button
+    Friend WithEvents bsub As Button
+    Friend WithEvents bad As Button
+    Friend WithEvents bequal As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,7 @@
 ﻿Public Class Form1
+    Dim FirstNumber As Long
+    Dim Operation As String
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -7,43 +10,131 @@
 
     End Sub
 
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles n1.Click
-        LCD.Text = LCD.Text & "1"
+        If LCD.Text = "0" Then
+            LCD.Text = "1"
+        Else
+            LCD.Text = LCD.Text & "1"
+        End If
     End Sub
 
     Private Sub n2_Click(sender As Object, e As EventArgs) Handles n2.Click
-        LCD.Text = LCD.Text & "2"
+        If LCD.Text = "0" Then
+            LCD.Text = "2"
+        Else
+            LCD.Text = LCD.Text & "2"
+        End If
     End Sub
 
     Private Sub n3_Click(sender As Object, e As EventArgs) Handles n3.Click
-        LCD.Text = LCD.Text & "3"
+        If LCD.Text = "0" Then
+            LCD.Text = "3"
+        Else
+            LCD.Text = LCD.Text & "3"
+        End If
     End Sub
 
+
     Private Sub n4_Click(sender As Object, e As EventArgs) Handles n4.Click
-        LCD.Text = LCD.Text & "4"
+        If LCD.Text = "0" Then
+            LCD.Text = "4"
+        Else
+            LCD.Text = LCD.Text & "4"
+        End If
     End Sub
 
     Private Sub n5_Click(sender As Object, e As EventArgs) Handles n5.Click
-        LCD.Text = LCD.Text & "5"
+        If LCD.Text = "0" Then
+            LCD.Text = "5"
+        Else
+            LCD.Text = LCD.Text & "5"
+        End If
     End Sub
 
     Private Sub n6_Click(sender As Object, e As EventArgs) Handles n6.Click
-        LCD.Text = LCD.Text & "6"
+        If LCD.Text = "0" Then
+            LCD.Text = "6"
+        Else
+            LCD.Text = LCD.Text & "6"
+        End If
     End Sub
 
     Private Sub n7_Click(sender As Object, e As EventArgs) Handles n7.Click
-        LCD.Text = LCD.Text & "7"
+        If LCD.Text = "0" Then
+            LCD.Text = "7"
+        Else
+            LCD.Text = LCD.Text & "7"
+        End If
     End Sub
 
     Private Sub n8_Click(sender As Object, e As EventArgs) Handles n8.Click
-        LCD.Text = LCD.Text & "8"
+        If LCD.Text = "0" Then
+            LCD.Text = "8"
+        Else
+            LCD.Text = LCD.Text & "8"
+        End If
     End Sub
 
     Private Sub n9_Click(sender As Object, e As EventArgs) Handles n9.Click
-        LCD.Text = LCD.Text & "9"
+        If LCD.Text = "0" Then
+            LCD.Text = "9"
+        Else
+            LCD.Text = LCD.Text & "9"
+        End If
     End Sub
 
     Private Sub n0_Click(sender As Object, e As EventArgs) Handles n0.Click
         LCD.Text = LCD.Text & "0"
+
+    End Sub
+
+
+    Private Sub bc_Click(sender As Object, e As EventArgs) Handles bc.Click
+        LCD.Text = "0"
+    End Sub
+
+    Private Sub bequal_Click(sender As Object, e As EventArgs) Handles bequal.Click
+        Dim SecondNumber As Long
+        Dim Result As Long
+
+        SecondNumber = LCD.Text
+
+        If Operation = "+" Then
+            Result = FirstNumber + SecondNumber
+        ElseIf Operation = "-" Then
+            Result = FirstNumber - SecondNumber
+        ElseIf Operation = "*" Then
+            Result = FirstNumber * SecondNumber
+        ElseIf Operation = "/" Then
+            Result = FirstNumber / SecondNumber
+        End If
+        FirstNumber = Result
+        LCD.Text = Result
+
+    End Sub
+
+    Private Sub bad_Click(sender As Object, e As EventArgs) Handles bad.Click
+        FirstNumber = LCD.Text
+        LCD.Text = "0"
+        Operation = "+"
+    End Sub
+
+    Private Sub bsub_Click(sender As Object, e As EventArgs) Handles bsub.Click
+        FirstNumber = LCD.Text
+        LCD.Text = "0"
+        Operation = "-"
+    End Sub
+
+    Private Sub bmult_Click(sender As Object, e As EventArgs) Handles bmult.Click
+        FirstNumber = LCD.Text
+        LCD.Text = "0"
+        Operation = "*"
+    End Sub
+
+    Private Sub bdiv_Click(sender As Object, e As EventArgs) Handles bdiv.Click
+        FirstNumber = LCD.Text
+        LCD.Text = "0"
+        Operation = "/"
     End Sub
 End Class
